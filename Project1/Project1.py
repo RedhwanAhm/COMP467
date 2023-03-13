e67a7348b83dbfe20df27d987f1aaab1efd8c48f
Redhwan Ahmed <redhwan.ah1202@gmail.com>
2023-03-12 21:01:06 -0700

    proj 1 changes

---
+++ Project1/Project1.py
@@ -0,0 +1,24 @@
+# Redhwan Ahmed
+# COMP 467
+# Project 1
+
+import argparse
+import sys
+
+#Parse arguments for job
+parser = argparse.ArgumentParser()
+parser.add_argument("--job", dest="jobFolder", help="job to process")
+parser.add_argument("--verbose", action="store_true", help="show verbose")
+parser.add_argument("--TC", dest="timecode", help="Timecode to process")
+
+args = parser.parse_args()
+
+if args.jobFolder is None:
+    print("No job selected")
+    sys.exit(2)
+else:
+    job = args.jobFolder
+
+if args.timecode:
+    timecodeTC = args.timecode
+
